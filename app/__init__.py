a3b3e90a2ff08260763fc529ee59c77bf6c92198
ElianaLara <c3074085@newcastle.ac.uk>
2026-02-09 17:29:50 -0600

    add log in route and html

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from config import Config
 
 def create_app():
     app = Flask(__name__)
 
     from .routes import main
     app.register_blueprint(main)
+    app.config.from_object(Config)
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,28 @@
-from flask import Blueprint
+from flask import render_template, redirect, url_for, session, Blueprint, flash, request
+
+from app.forms import LoginForm
 
 main = Blueprint("main", __name__)
 
-@main.route("/")
-def home():
-    return "none"
+
+@main.route('/')
+def dashboard():
+    if 'user' not in session:
+        return redirect(url_for('main.login'))
+
+    return render_template('dashboard.html')
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        # Example check (replace with DB logic)
+        if form.email.data == "test@test.com" and form.password.data == "1234":
+            session['user'] = form.email.data
+            flash("Logged in successfully!", "success")
+            return redirect(url_for('main.dashboard'))
+
+        flash("Invalid credentials", "danger")
+
+    return render_template('login.html', form=form)
